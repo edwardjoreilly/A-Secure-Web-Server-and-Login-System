91e16b334a377fe2d2f986d61265f8bf54ab2cfd
Justin Kingston <justin.kingston.414@my.csun.edu>
2021-04-25 07:02:27 +0000

    Upload New File

---
+++ sentMail.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Test</title>
+        <meta charset = "UTF-8">
+        <style>
+            #usernameError
+            {
+                width: 29.1%;
+                background-color: tomato;
+                font-weight: bold;
+                margin-top: 1%;
+                margin-bottom: 1%;
+    	    }
+        </style>
+    </head>
+    <body>
+        <h1>E-mail successfully sent!</h1>
+    </body>
+</html>
