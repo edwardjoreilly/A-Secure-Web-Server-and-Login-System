944840d0d03a634bfe3b22710de8670c1851a2e5
Justin Kingston <justin.kingston.414@my.csun.edu>
2021-04-30 20:20:52 +0000

    Replace forgotUserOrPass.php

--- forgotUserOrPass.php
+++ forgotUserOrPass.php
@@ -1,6 +1,59 @@
 <!--get query from databse for 1 of 3 security questions and a seperate query for their email using php-->
 <!--send them their login info on verification of credentials using mailto in html and getting the infor from the database query using php-->
+<!--EZ Clap-->
+<?php
+    session_start();
 
+    //Connect to the database
+    $dbHandle = mysqli_connect("localhost", "root", "(password)", "(database name)");
+
+    //Check database connection
+    if(!$dbHandle) {
+	    print("Could not connect to the database.");
+	    print(mysqli_connect_error());
+
+	    die(); //Kills process if unable to connect to the database
+    }
+
+    $correctAnswer = false;
+
+    if(isset($_POST["submit"]))
+    {
+        if($_POST["securityq1"] != '')
+        {
+            //Query DB for question answer. If true, set $correctAnswer to true.
+            //$correctAnswer = true;
+        }
+        else if($_POST["securityq2"] != '')
+        {
+            //Query DB for question answer. If true, set $correctAnswer to true.
+            //$correctAnswer = true;
+        }
+        else if($_POST["security3"] != '')
+        {
+            //Query DB for question answer. If true, set $correctAnswer to true.
+            //$correctAnswer = true;
+        }
+        else
+        {
+            echo "You have to answer at least one question.";
+        }
+    }
+
+    if($correctAnswer)
+    {
+        $_SESSION["EMAIL"] = $_POST["email"];
+        //Query DB for username
+        //$_SESSION["username"] = ;
+        $_SESSION["SUBJECT"] = "Forgot username or password";
+        //Query DB for password
+        //$_SESSION["password"] = ;
+        //$_SESSION["Body"] = "You are receiving this email because you forgot your username and password.\nUsername: " . $_SESSION["username"] . "\nPassword: " . $_SESSION["password"];
+        $_SESSION["hadForgotten"] = true;
+        $_SESSION["redirect"] = "location: http://10.0.2.15/test.php";
+        header("location: http://10.0.2.15/sendMail.php");
+    }
+?>
 
 <!DOCTYPE html>
 <html>
