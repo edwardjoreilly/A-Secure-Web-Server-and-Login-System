a21e2b880a04d8bb3df23e1cddb9873888136d94
Allan Huidobro <allanhuidobro@gmail.com>
2021-05-01 12:18:17 -0700

    forgot usernam and password done

--- src/forgotUserOrPass.php
+++ src/forgotUserOrPass.php
@@ -15,24 +15,57 @@
 	    die(); //Kills process if unable to connect to the database
     }
 
-    $correctAnswer = false;
-
+    
     if(isset($_POST["submit"]))
     {
+
+        $username = $_SESSION['username'];
+        $email = $_POST['email'];
+        $query4 = "SELECT * FROM users WHERE user_email='$email'";
+        //$query2 = mysqli_fetch_array($query2);
+        //mysqli_select_db('users');
+        $retval = mysqli_query($dbHandle, $query4);
+
+        if(! $retval ) {
+            die('Could not get data: ' . mysqli_error());
+        }
+        //$row = mysql_fetch_assoc($retval);
+        while($row = mysqli_fetch_assoc($retval)){
+            $pasword = $row['user_password'];
+            $securityq1 = $row['securityq1'];
+            $securityq2 = $row['securityq2'];
+            $securityq3 = $row['securityq3'];
+        }
+        $correctAnswer = false;
+
+
         if($_POST["securityq1"] != '')
         {
             //Query DB for question answer. If true, set $correctAnswer to true.
             //$correctAnswer = true;
+            if($_POST["securityq1"] == $securityq1)
+            {
+                $correctAnswer = true;
+            }
+
         }
         else if($_POST["securityq2"] != '')
         {
             //Query DB for question answer. If true, set $correctAnswer to true.
             //$correctAnswer = true;
+            if($_POST["securityq3"] == $securityq3)
+            {
+                $correctAnswer = true;
+            }
         }
         else if($_POST["security3"] != '')
         {
             //Query DB for question answer. If true, set $correctAnswer to true.
             //$correctAnswer = true;
+            if($_POST["securityq3"] == $securityq3)
+            {
+                $correctAnswer = true;
+            }
         }
         else
         {
@@ -44,13 +77,13 @@
     {
         $_SESSION["EMAIL"] = $_POST["email"];
         //Query DB for username
-        //$_SESSION["username"] = ;
+        $_SESSION["username"] = $username;
         $_SESSION["SUBJECT"] = "Forgot username or password";
         //Query DB for password
-        //$_SESSION["password"] = ;
-        //$_SESSION["Body"] = "You are receiving this email because you forgot your username and password.\nUsername: " . $_SESSION["username"] . "\nPassword: " . $_SESSION["password"];
+        $_SESSION["password"] = $pasword;
+        $_SESSION["Body"] = "You are receiving this email because you forgot your username and password.\nUsername: " . $_SESSION["username"] . "\nPassword: " . $_SESSION["password"];
         $_SESSION["hadForgotten"] = true;
-        $_SESSION["redirect"] = "location: http://172.18.30.210/test.php";
+        $_SESSION["redirect"] = "location: http://172.18.30.210/login.php";
         header("location: http://172.18.30.210/sendMail.php");
     }
 ?>
