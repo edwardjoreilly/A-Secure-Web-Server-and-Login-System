e3ac83d9dd178bb64531165fa1ffc5c91edb0555
My Tran <my.tran.811@my.csun.edu>
2021-05-01 05:23:48 +0000

    Uploading sentMail.php file

---
+++ src/sentMail.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+
+    if(isset($_POST["submit"]) && $_SESSION["code"] == $_POST["code"])
+    {
+        header("location: http://172.18.30.210/regSuccess.php");
+    }
+    else if(isset($_POST["submit"]) && $_SESSION["code"] != $_POST["code"])
+    {
+        echo "Try again.";
+    }
+?>
+
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Confirmation</title>
+        <meta charset = "UTF-8">
+        <style>
+            #usernameError
+            {
+                width: 29.1%;
+                background-color: tomato;
+                font-weight: bold;
+                margin-top: 1%;
+                margin-bottom: 1%;
+    	    }
+        </style>
+    </head>
+    <body>
+        <h1>Do not close this page!</h1>
+        <h3>Please enter the 6-digit code that was sent to your email to continue.</h5>
+        <form name="form" method="post" action="http://172.18.30.210/sentMail.php">
+            <label for="codeField">Code:</label><br>
+            <input type="text" id="codeField" name="code"><br><br>
+            <input type="submit" id="submitButton" name="submit" value="Submit">
+        </form>
+    </body>
+</html>
